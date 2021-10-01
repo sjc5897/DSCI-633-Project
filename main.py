efe85b8d0a1806974750982639b187d55d27ea10
Stephen Cook <stevejcook114@gmail.com>
2021-10-01 18:37:56 -0400

    First Commit: Added the data file as a csv file. Created very basic python script to load in datafile and print shape

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+DSCI-633 Project, Data Exploration Phase
+
+This is a python script using pandas that will help us explore some cool data
+
+@author:    Stephen Cook <sjc5897@rit.edu>
+@language:  Python 3
+@created:   10/1/21
+@last edit: 10/1/21
+"""
+
+# Imports
+import pandas as pd
+import numpy as np
+# /Imports
+
+"""
+Main function, loads in the file into pandas
+"""
+if __name__ == '__main__':
+    filename = "mental_health_data.csv"  # our data file
+
+    # read the csv, df (dataframe) is type pandas.dataframe
+    df = pd.read_csv('mental_health_data.csv')
+
+    # print the df (dataframes)
+    print(df.shape)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
