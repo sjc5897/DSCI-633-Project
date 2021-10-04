6fe94e6846f2c41f9027bab34c222ab302f4eeef
Stephen Cook <stevejcook114@gmail.com>
2021-10-04 14:53:58 -0400

    added the printing of column information

--- main.py
+++ main.py
@@ -12,7 +12,32 @@ This is a python script using pandas that will help us explore some cool data
 # Imports
 import pandas as pd
 import numpy as np
+
 # /Imports
+
+"""
+Simply prints the shape of the file, meaning the number of rows (entities) and columns (attributes)
+
+@input:     df  -   dataframe, the dataframe that you wish to be described
+@output:    void
+"""
+def print_shape(df):
+    row, columns = df.shape
+    print("Data Shape (Row Count and Column Count):\n \trows: " + str(row) + " column: " + str(columns))
+
+
+"""
+Prints out the column information, currently the value counts of each column
+
+@input      df  -   dataframe, the dataframe that you wish to be described
+@output     void
+"""
+def print_column_information(df):
+    print("Column Information:")
+    column_array = df.columns
+    for column_name in column_array:
+        print(str(df[column_name].value_counts()))
+
 
 """
 Main function, loads in the file into pandas
@@ -24,6 +49,7 @@ if __name__ == '__main__':
     df = pd.read_csv('mental_health_data.csv')
 
     # print the df (dataframes)
-    print(df.shape)
-    print(df)
+    print_shape(df)
 
+    # print column data
+    print_column_information(df)
